087011a1a784c73558d21b6a61754713900db48f
glowcoil <micahrjohnston@gmail.com>
2023-01-02 16:38:00 -0600

    tests for f32 SIMD operations

--- src/simd/mod.rs
+++ src/simd/mod.rs
@@ -66,3 +66,146 @@ pub trait Task {
 
     fn run<A: Arch>(self) -> Self::Result;
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    struct TestF32;
+
+    impl Task for TestF32 {
+        type Result = ();
+
+        #[inline(always)]
+        fn run<A: Arch>(self) {
+            let values = [
+                -3.0,
+                -2.0,
+                -1.0,
+                -0.0,
+                0.0,
+                1.0,
+                2.0,
+                3.0,
+                -f32::EPSILON,
+                f32::EPSILON,
+                f32::MIN,
+                f32::MAX,
+                f32::NEG_INFINITY,
+                f32::INFINITY,
+                f32::NAN,
+            ]
+            .into_iter()
+            .cycle()
+            .take(64)
+            .collect::<Vec<f32>>();
+
+            for chunk_a in values.chunks(A::f32::LANES) {
+                for chunk_b in values.chunks(A::f32::LANES) {
+                    let result = A::f32::from_slice(chunk_a) + A::f32::from_slice(chunk_b);
+                    for ((&a, &b), &c) in chunk_a
+                        .iter()
+                        .zip(chunk_b.iter())
+                        .zip(result.as_slice().iter())
+                    {
+                        let correct = a + b;
+                        assert!(
+                            correct.to_bits() == c.to_bits(),
+                            "expected {a} + {b} == {correct}, got {c}"
+                        );
+                    }
+
+                    let result = A::f32::from_slice(chunk_a) - A::f32::from_slice(chunk_b);
+                    for ((&a, &b), &c) in chunk_a
+                        .iter()
+                        .zip(chunk_b.iter())
+                        .zip(result.as_slice().iter())
+                    {
+                        let correct = a - b;
+                        assert!(
+                            correct.to_bits() == c.to_bits(),
+                            "expected {a} - {b} == {correct}, got {c}"
+                        );
+                    }
+
+                    let result = A::f32::from_slice(chunk_a) * A::f32::from_slice(chunk_b);
+                    for ((&a, &b), &c) in chunk_a
+                        .iter()
+                        .zip(chunk_b.iter())
+                        .zip(result.as_slice().iter())
+                    {
+                        let correct = a * b;
+                        assert!(
+                            correct.to_bits() == c.to_bits(),
+                            "expected {a} * {b} == {correct}, got {c}"
+                        );
+                    }
+
+                    let result = A::f32::from_slice(chunk_a) / A::f32::from_slice(chunk_b);
+                    for ((&a, &b), &c) in chunk_a
+                        .iter()
+                        .zip(chunk_b.iter())
+                        .zip(result.as_slice().iter())
+                    {
+                        let correct = a / b;
+                        assert!(
+                            correct.to_bits() == c.to_bits(),
+                            "expected {a} / {b} == {correct}, got {c}"
+                        );
+                    }
+                }
+            }
+
+            for chunk in values.chunks(A::f32::LANES) {
+                let result = -A::f32::from_slice(chunk);
+                for (&a, &b) in chunk.iter().zip(result.as_slice().iter()) {
+                    assert!((-a).to_bits() == b.to_bits(), "-{a} == {b}");
+                }
+            }
+
+            // Test for scan_sum. Since different backends use different reduction trees, we can't
+            // expect bit equality in the face of rounding or edge cases like NaN or infinity, so
+            // just test on exactly representable integers for now.
+
+            let values = (0..64).map(|x| x as f32).collect::<Vec<f32>>();
+
+            for chunk in values.chunks(A::f32::LANES) {
+                let result = A::f32::from_slice(chunk).scan_sum();
+                let mut correct = A::f32::from_slice(chunk);
+                let mut accum = 0.0;
+                for x in correct.as_mut_slice() {
+                    accum += *x;
+                    *x = accum;
+                }
+                let equal = result
+                    .as_slice()
+                    .iter()
+                    .zip(correct.as_slice().iter())
+                    .all(|(a, b)| a == b);
+                assert!(equal, "scan_sum() failed\n   input: {chunk:?}\nexpected: {correct:?}\n     got: {result:?}");
+            }
+        }
+    }
+
+    #[test]
+    fn scalar_f32() {
+        let test = Scalar::specialize::<TestF32>();
+        test(TestF32);
+    }
+
+    #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
+    #[test]
+    fn sse2_f32() {
+        if let Some(test) = Sse2::try_specialize::<TestF32>() {
+            test(TestF32);
+        }
+    }
+
+    #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
+    #[test]
+    fn avx2_f32() {
+        if let Some(test) = Avx2::try_specialize::<TestF32>() {
+            test(TestF32);
+        }
+    }
+}
