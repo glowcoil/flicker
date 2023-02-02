85863dcf7ac578bca45bc96734a028e5aab211ef
micah <micah@glowcoil.com>
2023-02-01 22:29:06 -0600

    factor out svg loading code from bench and example into shared crate

--- examples/svg.rs
+++ examples/svg.rs
@@ -1,106 +1,11 @@
 use std::cell::RefCell;
 use std::time::Duration;
 
-use flicker::{Canvas, Color, Font, Mat2x2, Path, Transform, Vec2};
+use flicker::{Canvas, Color, Font, Transform};
 use portlight::{Application, Rect, Window, WindowHandler, WindowOptions};
 
 const WIDTH: usize = 1024;
 const HEIGHT: usize = 1024;
-
-enum Style {
-    Fill,
-    Stroke(f32),
-}
-
-struct Command {
-    path: Path,
-    style: Style,
-    color: Color,
-}
-
-fn build_list(node: &usvg::Node, commands: &mut Vec<Command>) {
-    use usvg::NodeExt;
-    match *node.borrow() {
-        usvg::NodeKind::Path(ref p) => {
-            let t = node.transform();
-            let transform = Transform::new(
-                Mat2x2::new(t.a as f32, t.c as f32, t.b as f32, t.d as f32),
-                Vec2::new(t.e as f32, t.f as f32),
-            );
-
-            let mut path = Path::new();
-            for segment in p.data.0.iter() {
-                match *segment {
-                    usvg::PathSegment::MoveTo { x, y } => {
-                        path.move_to(transform.apply(Vec2::new(x as f32, y as f32)));
-                    }
-                    usvg::PathSegment::LineTo { x, y } => {
-                        path.line_to(transform.apply(Vec2::new(x as f32, y as f32)));
-                    }
-                    usvg::PathSegment::CurveTo {
-                        x1,
-                        y1,
-                        x2,
-                        y2,
-                        x,
-                        y,
-                    } => {
-                        path.cubic_to(
-                            transform.apply(Vec2::new(x1 as f32, y1 as f32)),
-                            transform.apply(Vec2::new(x2 as f32, y2 as f32)),
-                            transform.apply(Vec2::new(x as f32, y as f32)),
-                        );
-                    }
-                    usvg::PathSegment::ClosePath => {
-                        path.close();
-                    }
-                }
-            }
-
-            if let Some(ref fill) = p.fill {
-                if let usvg::Paint::Color(color) = fill.paint {
-                    let color =
-                        Color::rgba(color.red, color.green, color.blue, fill.opacity.to_u8());
-                    commands.push(Command {
-                        path: path.clone(),
-                        style: Style::Fill,
-                        color,
-                    });
-                }
-            }
-
-            if let Some(ref stroke) = p.stroke {
-                if let usvg::Paint::Color(color) = stroke.paint {
-                    let color =
-                        Color::rgba(color.red, color.green, color.blue, stroke.opacity.to_u8());
-                    commands.push(Command {
-                        path,
-                        style: Style::Stroke(stroke.width.value() as f32),
-                        color,
-                    });
-                }
-            }
-        }
-        _ => {}
-    }
-
-    for child in node.children() {
-        build_list(&child, commands);
-    }
-}
-
-fn render(commands: &[Command], canvas: &mut Canvas) {
-    for command in commands {
-        match command.style {
-            Style::Fill => {
-                canvas.fill_path(&command.path, &Transform::scale(2.0), command.color);
-            }
-            Style::Stroke(width) => {
-                canvas.stroke_path(&command.path, width, &Transform::scale(2.0), command.color);
-            }
-        }
-    }
-}
 
 const AVERAGE_WINDOW: usize = 64;
 
@@ -143,12 +48,12 @@ impl FrameTimer {
 struct Handler {
     canvas: RefCell<Canvas>,
     font: Font,
-    commands: Vec<Command>,
+    commands: Vec<svg::Command>,
     timer: RefCell<FrameTimer>,
 }
 
 impl Handler {
-    fn new(commands: Vec<Command>) -> Handler {
+    fn new(commands: Vec<svg::Command>) -> Handler {
         Handler {
             canvas: RefCell::new(Canvas::with_size(WIDTH, HEIGHT)),
             font: Font::from_bytes(include_bytes!("res/SourceSansPro-Regular.otf"), 0).unwrap(),
@@ -169,7 +74,7 @@ impl WindowHandler for Handler {
             .clear(Color::rgba(255, 255, 255, 255));
 
         let time = std::time::Instant::now();
-        render(&self.commands, &mut *self.canvas.borrow_mut());
+        svg::render(&self.commands, &mut *self.canvas.borrow_mut());
         let elapsed = time.elapsed();
 
         self.timer.borrow_mut().update(elapsed);
@@ -201,10 +106,7 @@ fn main() {
         .as_ref()
         .map(|s| &s[..])
         .unwrap_or("examples/res/tiger.svg");
-    let tree = usvg::Tree::from_file(path, &usvg::Options::default()).unwrap();
-
-    let mut commands = Vec::new();
-    build_list(&tree.root(), &mut commands);
+    let commands = svg::from_file(path).unwrap();
 
     let app = Application::new().unwrap();
 
---
+++ svg/src/lib.rs
@@ -0,0 +1,108 @@
+use std::error::Error;
+use std::path;
+
+use flicker::{Canvas, Color, Mat2x2, Path, Transform, Vec2};
+
+pub enum Style {
+    Fill,
+    Stroke(f32),
+}
+
+pub struct Command {
+    pub path: Path,
+    pub style: Style,
+    pub color: Color,
+}
+
+pub fn from_file<P: AsRef<path::Path>>(path: P) -> Result<Vec<Command>, Box<dyn Error>> {
+    let tree = usvg::Tree::from_file(path, &usvg::Options::default())?;
+
+    let mut commands = Vec::new();
+    build_list(&tree.root(), &mut commands);
+
+    Ok(commands)
+}
+
+fn build_list(node: &usvg::Node, commands: &mut Vec<Command>) {
+    use usvg::NodeExt;
+    match *node.borrow() {
+        usvg::NodeKind::Path(ref p) => {
+            let t = node.transform();
+            let transform = Transform::new(
+                Mat2x2::new(t.a as f32, t.c as f32, t.b as f32, t.d as f32),
+                Vec2::new(t.e as f32, t.f as f32),
+            );
+
+            let mut path = Path::new();
+            for segment in p.data.0.iter() {
+                match *segment {
+                    usvg::PathSegment::MoveTo { x, y } => {
+                        path.move_to(transform.apply(Vec2::new(x as f32, y as f32)));
+                    }
+                    usvg::PathSegment::LineTo { x, y } => {
+                        path.line_to(transform.apply(Vec2::new(x as f32, y as f32)));
+                    }
+                    usvg::PathSegment::CurveTo {
+                        x1,
+                        y1,
+                        x2,
+                        y2,
+                        x,
+                        y,
+                    } => {
+                        path.cubic_to(
+                            transform.apply(Vec2::new(x1 as f32, y1 as f32)),
+                            transform.apply(Vec2::new(x2 as f32, y2 as f32)),
+                            transform.apply(Vec2::new(x as f32, y as f32)),
+                        );
+                    }
+                    usvg::PathSegment::ClosePath => {
+                        path.close();
+                    }
+                }
+            }
+
+            if let Some(ref fill) = p.fill {
+                if let usvg::Paint::Color(color) = fill.paint {
+                    let color =
+                        Color::rgba(color.red, color.green, color.blue, fill.opacity.to_u8());
+                    commands.push(Command {
+                        path: path.clone(),
+                        style: Style::Fill,
+                        color,
+                    });
+                }
+            }
+
+            if let Some(ref stroke) = p.stroke {
+                if let usvg::Paint::Color(color) = stroke.paint {
+                    let color =
+                        Color::rgba(color.red, color.green, color.blue, stroke.opacity.to_u8());
+                    commands.push(Command {
+                        path,
+                        style: Style::Stroke(stroke.width.value() as f32),
+                        color,
+                    });
+                }
+            }
+        }
+        _ => {}
+    }
+
+    for child in node.children() {
+        build_list(&child, commands);
+    }
+}
+
+pub fn render(commands: &[Command], canvas: &mut Canvas) {
+    for command in commands {
+        match command.style {
+            Style::Fill => {
+                canvas.fill_path(&command.path, &Transform::scale(2.0), command.color);
+            }
+            Style::Stroke(width) => {
+                canvas.stroke_path(&command.path, width, &Transform::scale(2.0), command.color);
+            }
+        }
+    }
+}
