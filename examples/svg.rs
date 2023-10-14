ab7d5722c22b6b943344df5c682301846dcc1dec
Micah Johnston <micahrjohnston@gmail.com>
2023-10-14 14:44:04 -0500

    update svg example to newer version of portlight

--- examples/svg.rs
+++ examples/svg.rs
@@ -1,11 +1,13 @@
-use std::cell::{Cell, RefCell};
 use std::time::Duration;
 
 use flicker::{Canvas, Color, Font, Transform, Vec2};
-use portlight::{Application, MouseButton, Point, Rect, Window, WindowHandler, WindowOptions};
+use portlight::{
+    AppContext, AppOptions, Bitmap, Event, MouseButton, Point, Response, Size, Window,
+    WindowOptions,
+};
 
-const WIDTH: usize = 1024;
-const HEIGHT: usize = 1024;
+const WIDTH: usize = 512;
+const HEIGHT: usize = 512;
 
 const AVERAGE_WINDOW: usize = 32;
 
@@ -45,115 +47,113 @@ impl FrameTimer {
     }
 }
 
-struct Handler {
-    canvas: RefCell<Canvas>,
+struct State {
+    scale: f64,
+    canvas: Canvas,
     font: Font,
     commands: Vec<svg::Command>,
-    timer: RefCell<FrameTimer>,
-    mouse_pos: Cell<Point>,
-    dragging: Cell<bool>,
-    transform: Cell<Transform>,
+    timer: FrameTimer,
+    mouse_pos: Point,
+    dragging: bool,
+    transform: Transform,
+    window: Window,
 }
 
-impl Handler {
-    fn new(commands: Vec<svg::Command>) -> Handler {
-        Handler {
-            canvas: RefCell::new(Canvas::with_size(WIDTH, HEIGHT)),
+impl State {
+    fn new(commands: Vec<svg::Command>, window: Window) -> State {
+        let scale = window.scale();
+        let width = WIDTH as f64 * scale;
+        let height = HEIGHT as f64 * scale;
+
+        State {
+            scale,
+            canvas: Canvas::with_size(width as usize, height as usize),
             font: Font::from_bytes(include_bytes!("res/SourceSansPro-Regular.otf"), 0).unwrap(),
             commands,
-            timer: RefCell::new(FrameTimer::new()),
-            mouse_pos: Cell::new(Point { x: -1.0, y: -1.0 }),
-            dragging: Cell::new(false),
-            transform: Cell::new(Transform::id()),
+            timer: FrameTimer::new(),
+            mouse_pos: Point { x: -1.0, y: -1.0 },
+            dragging: false,
+            transform: Transform::id(),
+            window,
         }
     }
-}
 
-impl WindowHandler for Handler {
-    fn frame(&self, window: &Window) {
-        window.request_display();
-    }
+    fn handle_event(&mut self, cx: &AppContext<Self>, event: Event) -> Response {
+        match event {
+            Event::Frame => {
+                self.canvas.clear(Color::rgba(255, 255, 255, 255));
 
-    fn display(&self, window: &Window) {
-        self.canvas
-            .borrow_mut()
-            .clear(Color::rgba(255, 255, 255, 255));
+                let time = std::time::Instant::now();
+                svg::render(
+                    &self.commands,
+                    &self.transform.then(Transform::scale(self.scale as f32)),
+                    &mut self.canvas,
+                );
+                let elapsed = time.elapsed();
 
-        let time = std::time::Instant::now();
-        svg::render(
-            &self.commands,
-            &self.transform.get(),
-            &mut *self.canvas.borrow_mut(),
-        );
-        let elapsed = time.elapsed();
+                self.timer.update(elapsed);
 
-        self.timer.borrow_mut().update(elapsed);
+                self.canvas.fill_text(
+                    &format!("{:#.3?}", self.timer.average()),
+                    &self.font,
+                    24.0,
+                    &Transform::scale(self.scale as f32),
+                    Color::rgba(0, 0, 0, 255),
+                );
 
-        self.canvas.borrow_mut().fill_text(
-            &format!("{:#.3?}", self.timer.borrow().average()),
-            &self.font,
-            24.0,
-            &Transform::id(),
-            Color::rgba(0, 0, 0, 255),
-        );
+                self.window.present(Bitmap::new(
+                    self.canvas.data(),
+                    self.canvas.width(),
+                    self.canvas.height(),
+                ));
+            }
+            Event::MouseMove(pos) => {
+                if self.dragging {
+                    let prev = Vec2::new(self.mouse_pos.x as f32, self.mouse_pos.y as f32);
+                    let curr = Vec2::new(pos.x as f32, pos.y as f32);
+                    self.transform = self
+                        .transform
+                        .then(Transform::translate(curr.x - prev.x, curr.y - prev.y));
+                }
 
-        window.update_contents(
-            self.canvas.borrow().data(),
-            self.canvas.borrow().width(),
-            self.canvas.borrow().height(),
-        );
-    }
+                self.mouse_pos = pos;
+            }
+            Event::MouseDown(btn) => {
+                if btn == MouseButton::Left {
+                    self.dragging = true;
+                }
 
-    fn mouse_move(&self, _window: &Window, position: Point) {
-        if self.dragging.get() {
-            let prev = Vec2::new(self.mouse_pos.get().x as f32, self.mouse_pos.get().y as f32);
-            let curr = Vec2::new(position.x as f32, position.y as f32);
-            self.transform.set(
-                self.transform
-                    .get()
-                    .then(Transform::translate(curr.x - prev.x, curr.y - prev.y)),
-            );
+                return Response::Capture;
+            }
+            Event::MouseUp(btn) => {
+                if btn == MouseButton::Left {
+                    self.dragging = false;
+                }
+
+                return Response::Capture;
+            }
+            Event::Scroll(delta) => {
+                self.transform = self
+                    .transform
+                    .then(Transform::translate(
+                        -self.mouse_pos.x as f32,
+                        -self.mouse_pos.y as f32,
+                    ))
+                    .then(Transform::scale(1.02f32.powf(delta.y as f32)))
+                    .then(Transform::translate(
+                        self.mouse_pos.x as f32,
+                        self.mouse_pos.y as f32,
+                    ));
+
+                return Response::Capture;
+            }
+            Event::Close => {
+                cx.exit();
+            }
+            _ => {}
         }
 
-        self.mouse_pos.set(position);
-    }
-
-    fn mouse_down(&self, _window: &Window, button: MouseButton) -> bool {
-        if button == MouseButton::Left {
-            self.dragging.set(true);
-        }
-
-        true
-    }
-
-    fn mouse_up(&self, _window: &Window, button: MouseButton) -> bool {
-        if button == MouseButton::Left {
-            self.dragging.set(false);
-        }
-
-        true
-    }
-
-    fn scroll(&self, _window: &Window, _dx: f64, dy: f64) -> bool {
-        let mouse_pos = self.mouse_pos.get();
-
-        self.transform.set(
-            self.transform
-                .get()
-                .then(Transform::translate(
-                    -mouse_pos.x as f32,
-                    -mouse_pos.y as f32,
-                ))
-                .then(Transform::scale(1.02f32.powf(dy as f32)))
-                .then(Transform::translate(mouse_pos.x as f32, mouse_pos.y as f32)),
-        );
-
-        true
-    }
-
-    fn request_close(&self, window: &Window) {
-        window.close().unwrap();
-        window.application().stop();
+        Response::Ignore
     }
 }
 
@@ -165,23 +165,25 @@ fn main() {
         .unwrap_or("examples/res/tiger.svg");
     let commands = svg::from_file(path).unwrap();
 
-    let app = Application::new().unwrap();
+    let mut app = AppOptions::new()
+        .build(|cx| {
+            let window = WindowOptions::new()
+                .title("flicker")
+                .size(Size::new(WIDTH as f64, HEIGHT as f64))
+                .open(cx, State::handle_event)
+                .unwrap();
 
-    Window::open(
-        &app,
-        WindowOptions {
-            title: "flicker".to_string(),
-            rect: Rect {
-                x: 0.0,
-                y: 0.0,
-                width: WIDTH as f64,
-                height: HEIGHT as f64,
-            },
-            handler: Box::new(Handler::new(commands)),
-            ..WindowOptions::default()
-        },
-    )
-    .unwrap();
+            let scale = window.scale();
+            let width = (WIDTH as f64 * scale) as usize;
+            let height = (HEIGHT as f64 * scale) as usize;
+            let framebuffer = vec![0xFFFF00FF; width * height];
+            window.present(Bitmap::new(&framebuffer, width, height));
 
-    app.start().unwrap();
+            window.show();
+
+            Ok(State::new(commands, window))
+        })
+        .unwrap();
+
+    app.run().unwrap();
 }
